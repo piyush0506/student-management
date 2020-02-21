88b6eba187ef03b100cad02c2dfc3f48c9f6819f
sahu05 <sahu.piyush05@gmail.com>
2020-02-22 01:45:30 +0530

    Changes for heroku deployment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(express.static('./'));
 
  
-var server = app.listen(8080, function(){
+var server = app.listen(process.env.PORT || 5000, function(){
     var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
 });
