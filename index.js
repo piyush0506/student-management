76a462aab51ee9241e298901e8db2bb7874da8c9
sahu05 <sahu.piyush05@gmail.com>
2020-02-22 01:36:02 +0530

    Changes for heroku deployment

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require("express");
+ 
+var app = express();
+ 
+app.use(express.static('AnaxeeFUT'));
+
+ 
+var server = app.listen(8080, function(){
+    var port = server.address().port;
+    console.log("Server started at http://localhost:%s", port);
+});
